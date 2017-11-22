f9f8779b6a4cbf520d15a939a99bb30f653719d0
Daniel Cech <daniel8cech@gmail.com>
2017-11-22 16:05:05 +0100

    done / hotovo

--- script.js
+++ script.js
@@ -24,10 +24,16 @@ let rightAnswer
 // indicates if computer showing the sequence in this moment
 let active = false
 
+function focus() {
+    numberInput.focus()
+}
+
 
 function enterStart(e) {
     if (e.keyCode === 13 || e.keyCode === 32) {
-        setNew()
+        if (startButton.disabled == false) {
+            setNew()
+        }
     }
 }
 function disableInput(value) {
@@ -67,20 +73,19 @@ function playSequence() {
     active = true
     let i = 0
 
-    let play = function() {
+    let play = function () {
         interval = setTimeout(play, 800)
-        if (sequence.length - 1  == i) {
+        if (sequence.length - 1 == i) {
             clearTimeout(interval)
         }
         show(sequence[i])
         playSound(sequence[i] % 4)
         i++
     }
-    
+
     play()
-    setTimeout(function(){
-        show('?')
-        number.classList.add('slowAnimation')
+    setTimeout(function () {
+        starGen()
         disableInput(false)
         numberInput.focus()
         header.textContent = 'memNum ...'
@@ -88,13 +93,26 @@ function playSequence() {
 }
 
 
+function starGen() {
+    let stars = []
+    for (i = 0; i < sequence.length - numberInput.value.length; i++) {
+        stars.push('*')
+    }
+
+    stars = stars.join('')
+    show(numberInput.value + stars)
+}
+
+
 function listen() {
     const typedNumber = numberInput.value
-    if (isNaN(typedNumber)) {
+    if (isNaN(typedNumber) || typedNumber == " ") {
         alert('Insert only whole numbers please.')
         numberInput.value = ""
         return
     }
+
+    starGen()
 
     if (sequence.length === 0) {
         return
@@ -104,7 +122,7 @@ function listen() {
             disableInput(true)
             coins.play()
             points++
-            setTimeout(function(){
+            setTimeout(function () {
                 start()
                 header.textContent = "memNum ..."
             }, 1500)
@@ -135,8 +153,8 @@ function exit() {
     wrongSound.play()
     clearInterval(interval)
     result.innerHTML = '<p style="color: #26c092">right answer: ' + rightAnswer + '<\p>' +
-                       '<p style="color: #c03d26">your answer: ' + wrongAnswer + '<\p>'
-    if(points != 1) {
+        '<p style="color: #c03d26">your answer: ' + wrongAnswer + '<\p>'
+    if (points != 1) {
         result.innerHTML += '<p>You have ' + points + ' points.</p>'
     } else {
         result.innerHTML += '<p>You have ' + points + ' point.</p>'
